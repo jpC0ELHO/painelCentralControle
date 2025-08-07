b6c602ea26fced72e2b6c50f822cc86986aa58da
jpc.human() <110858907+jpC0ELHO@users.noreply.github.com>
2025-08-07 02:27:01 -0300

    update EstacionamentoDia

--- src/main/java/com/geop/painelCentralControle/domain/entities/estacionamento/EstacionamentoDia.java
+++ src/main/java/com/geop/painelCentralControle/domain/entities/estacionamento/EstacionamentoDia.java
@@ -1,4 +1,46 @@
 package com.geop.painelCentralControle.domain.entities.estacionamento;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.time.LocalDateTime;
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "estacionamento_dia")
 public class EstacionamentoDia {
+    @Column
+    private Boolean ocupada;
+    @Column
+    private Integer tempoOcupacaoAtual;
+    @Column
+    private Integer quantidadeTrocasDia;
+    @Column
+    private Integer vagasOcupadas;
+    @Column
+    private Integer vagasLivres;
+    @Column
+    private String horarioPico;
+    @Column
+    private Integer tempoTotalLivre;
+    @Column
+    private Boolean vagasSubutilizadas;
+    @Column
+    private Boolean vagasCriticas;
+    @DateTimeFormat(pattern = "dd/MM/yyyy HH:mm:ss")
+    @JsonSerialize(using = LocalDateTimeSerializer.class)
+    @JsonDeserialize(using = LocalDateTimeDeserializer.class)
+    @JsonFormat(pattern = "dd/MM/yyyy HH:mm:ss", shape = JsonFormat.Shape.STRING)
+    private LocalDateTime dataHoraEvento;
 }
