2abac7a3ab635ddc2b9729f2e3a7412916887812
jpc.human() <110858907+jpC0ELHO@users.noreply.github.com>
2025-08-07 02:26:40 -0300

    update EstacionamentoMes

--- src/main/java/com/geop/painelCentralControle/domain/entities/estacionamento/EstacionamentoMes.java
+++ src/main/java/com/geop/painelCentralControle/domain/entities/estacionamento/EstacionamentoMes.java
@@ -1,4 +1,28 @@
 package com.geop.painelCentralControle.domain.entities.estacionamento;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "estacionamento_mes")
 public class EstacionamentoMes {
+    @Column
+    private Double taxaRotatividade;
+    @Column
+    private Double taxaOcupacao;
+    @Column
+    private String diaMaiorMovimento;
+    @Column
+    private Integer mediaDiariaEntradaVeiculos;
+    @Column
+    private LocalDateTime dataHoraEvento;
 }
